2d4f430c55f7f8136688b42eb4d6844100c2a527
a.chernenko <ac.chernenko@gmail.com>
2022-01-31 13:57:57 +0700

    Border jumping

--- paddleball.py
+++ paddleball.py
@@ -3,7 +3,6 @@ from model.ball import Ball
 import random
 import time
 
-
 tk = Tk()
 tk.title("Игра")
 tk.resizable(0, 0)
@@ -11,9 +10,11 @@ tk.wm_attributes("-topmost", 1)
 canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
+
 ball = Ball(canvas, "red")
 
 while 1:
+    ball.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
