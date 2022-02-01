631220e00cecf1c7647a6ec4b826674dc2ceca35
a.chernenko <ac.chernenko@gmail.com>
2022-02-01 11:33:41 +0700

    Finish game

--- model/ball.py
+++ model/ball.py
@@ -3,16 +3,25 @@ import random
 
 class Ball:
 
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, paddle, color):
         self.canvas = canvas
+        self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
         starts = [-3, -2, -1, 1, 2, 3]
         random.shuffle(starts)
         self.x = starts[0]
-        self.y = -1
+        self.y = -3
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
+        self.hit_bottom = False
+
+    def hit_paddle(self, pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                return True
+        return False
 
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
@@ -20,6 +29,8 @@ class Ball:
         if pos[1] <= 0:
             self.y = 3
         if pos[3] >= self.canvas_height:
+            self.hit_bottom = True
+        if self.hit_paddle(pos) == True:
             self.y = -3
         if pos[0] <= 0:
             self.x = 3
--- paddleball.py
+++ paddleball.py
@@ -1,7 +1,8 @@
 from tkinter import *
 from model.ball import Ball
-import random
+from model.paddle import Paddle
 import time
+
 
 tk = Tk()
 tk.title("Игра")
@@ -11,10 +12,13 @@ canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
 
-ball = Ball(canvas, "red")
+paddle = Paddle(canvas, 'blue')
+ball = Ball(canvas, paddle, 'red')
 
 while 1:
-    ball.draw()
+    if ball.hit_bottom == False:
+        ball.draw()
+        paddle.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
